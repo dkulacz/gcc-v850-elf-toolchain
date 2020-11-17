d3c6c16059821e9b3ccf0f746611d340fdc598cc
dkulacz <dkulacz>
2020-11-17 20:40:57 +0100

    Remove deprecated TODO

--- build-gcc-v850-elf-toolchain.sh
+++ build-gcc-v850-elf-toolchain.sh
@@ -101,8 +101,7 @@ make -w install-gcc 2>&1 | tee make.out
 mkdir -p ${BUILD_PATH}/newlib
 cd ${BUILD_PATH}/newlib
 
-export CFLAGS_FOR_TARGET="-Os" # todo: -DPREFER_SIZE_OVER_SPEED=1
-
+export CFLAGS_FOR_TARGET="-Os"
 ${SOURCES_PATH}/newlib-${NEWLIB_VERSION}/configure \
 --target=${TARGET_ARCH} \
 --prefix=${TOOLCHAIN_PATH} \
